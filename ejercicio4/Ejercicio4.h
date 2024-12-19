6b7c401ee2033367a7c636c63c25419c4eb2189f
BEATRIZ GARRIDO JARAMILLO <bgarrjar@myuax.com>
2024-12-19 10:02:52 +0100

    Ejercicio 4

---
+++ ejercicio4/Ejercicio4.h
@@ -0,0 +1,10 @@
+//
+// Created by trici on 19/12/2024.
+//
+
+#ifndef EJERCICIO4_H
+#define EJERCICIO4_H
+
+void calcularOperaciones();
+
+#endif //EJERCICIO4_H
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "ejercicio1/Ejercicio1.h"
 #include "ejercicio2/Ejercicio2.h"
 #include "ejercicio3/Ejercicio3.h"
+#include "ejercicio4/Ejercicio4.h"
 
 
 int main() {
@@ -11,5 +12,7 @@ int main() {
     manejarVector();
     manejarPila();
     ordenarYMaximo();
+    calcularOperaciones();
+
     return 0;
 }
